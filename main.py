647c90508265efcc79a10e111a4cb2373dc471a6
Sandijs-buls <C24456862@mytudublin.ie>
2026-07-08 22:54:00 +0100

    Getting an individual posts + logic

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import Optional
 from fastapi import Body, FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
@@ -11,7 +12,12 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
-  
+my_posts = [{"title" : "title of post 1", "content": "content of post 1", "id": 1}, {"title": "favorite foods", "content": "I like pizza", "id": 2}]
+
+def find_post(id):
+    for p in my_posts:
+        if p['id'] == id:
+            return p
 
 @app.get("/")
 async def root():
@@ -19,10 +25,21 @@ async def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is your posts"}
+    return {"data": my_posts}
 
-@app.post("/create_post")
-def create_posts(new_post: Post):
-    print(new_post)
-    print(new_post.dict())
-    return {"new_posts": new_post}
+@app.post("/posts")
+def create_posts(post: Post):
+    print(post)
+    print(post.dict())
+    
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    return {"new_posts": post_dict}
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+   
+    post = find_post(id)
+    print(post)
+    return {"post_detail": post}
