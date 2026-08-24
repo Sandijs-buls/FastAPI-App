cf61498c7eb183b0f84777e269e8ad6c2667cafc
Sandijs-buls <C24456862@mytudublin.ie>
2026-08-24 23:22:28 +0100

    Updated test_users file to not have the database information included in the test file.

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,43 +1,9 @@
-from fastapi.testclient import TestClient
-import pytest
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from app.database import SessionLocal
+
 from app.main import app
 from app import schemas
-from app.config import settings
-from app.database import get_db, Base
+from .database import client, session
 
 
-
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-Testing_SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)#
-
-
-Base.metadata.create_all(bind=engine)
-
-
-
-def override_get_db():
-    db = Testing_SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-app.dependency_overrides[get_db] = override_get_db
-
-
-
-@pytest.fixture
-def client():
-    Base.metadata.create_all(bind=engine)
-    yield TestClient(app)
-    Base.metadata.drop_all(bind=engine)
 
 
 
