53d1c55634ba92fa36b5fa1f9e94cb23b2f18641
Sandijs-buls <C24456862@mytudublin.ie>
2026-08-25 15:04:14 +0100

    Validate the token made by logging in with a test

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,5 +1,7 @@
 
 from app.main import app
+from app.config import settings
+from jose import jwt
 from app import schemas
 from .database import client, session
 import pytest
@@ -33,5 +35,13 @@ def test_create_user(client):
 
 def test_login_user( test_user, client):
     response = client.post("/login", data={"username": test_user['email'], "password": test_user['password']})
-    print(response.json())
+    login_res = schemas.Token(**response.json())
+
+    #Validating the token, by decoding it and checking if the user_id in the payload matches the test_user id
+    payload = jwt.decode(login_res.access_token, settings.secret_key, algorithms=[settings.algorithm])
+    id = payload.get("user_id")
+
+    assert id == test_user['id']
+    assert login_res.token_type == "bearer"
+
     assert response.status_code == 200
