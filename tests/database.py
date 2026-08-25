789387e293fba197c01da11caeabe16fd4f92348
Sandijs-buls <C24456862@mytudublin.ie>
2026-08-25 14:54:36 +0100

    Added a fixture to be able to login before we run our tests , specifically helpful for logging in

--- tests/database.py
+++ tests/database.py
@@ -19,7 +19,7 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 Testing_SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture()
 def session():
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
@@ -31,7 +31,7 @@ def session():
         db.close()
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture()
 def client(session):
 
     def override_get_db():
--- tests/test_users.py
+++ tests/test_users.py
@@ -2,6 +2,19 @@
 from app.main import app
 from app import schemas
 from .database import client, session
+import pytest
+
+
+@pytest.fixture()
+def test_user(client):
+    user_data = {"email" : "hello12@gmail.com", "password" : "1234"}
+    res = client.post("/users/", json=user_data)
+    assert res.status_code == 201
+
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+    return new_user
+
 
 
 
@@ -18,7 +31,7 @@ def test_create_user(client):
     assert response.status_code == 201
     assert user.email == "hello12@gmail.com"
 
-def test_login_user(client):
-    response = client.post("/login", data={"username": "hello12@gmail.com", "password": "1234"})
+def test_login_user( test_user, client):
+    response = client.post("/login", data={"username": test_user['email'], "password": test_user['password']})
     print(response.json())
     assert response.status_code == 200
